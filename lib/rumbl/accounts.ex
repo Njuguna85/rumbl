ae0b05c98d63989d9b8036b903eb852d0ae8894e
Dennis Njuguna <dennix.njuguna85@gmail.com>
2023-07-12 11:23:22 +0300

    add logout, add dynamic header

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -49,7 +49,7 @@ defmodule Rumbl.Accounts do
     user = get_user_by(username: username)
 
     cond do
-      user && Pbkdf2.verify_pass(given_pass, user.password_has) ->
+      user && Pbkdf2.verify_pass(given_pass, user.password_hash) ->
         {:ok, user}
 
       user ->
