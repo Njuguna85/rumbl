82ced8d92e5642c4ea82db72a8565a6732d127b1
Dennis Njuguna <dennix.njuguna85@gmail.com>
2023-07-27 15:57:10 +0300

    add youtube video widget

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -2,6 +2,7 @@ defmodule Rumbl.MultiMedia.Video do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, Rumbl.Multimedia.Permalink, autogenerate: true}
   schema "videos" do
     field :description, :string
     field :title, :string
@@ -18,5 +19,19 @@ defmodule Rumbl.MultiMedia.Video do
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
     |> assoc_constraint(:category)
+    |> slugify_title()
+  end
+
+  defp slugify_title(changeset) do
+    case fetch_change(changeset, :title) do
+      {:ok, new_title} -> put_change(changeset, :slug, slugify(new_title))
+      :error -> changeset
+    end
+  end
+
+  def slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
